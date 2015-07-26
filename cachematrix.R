10b855879fad4b04e81cb78efdeb11c17b26117b
wenting <wenshih@gmail.com>
2015-07-26 00:25:14 -0700

    07-26-15 first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can 
+##cache its inverse. The input is matrix x, and this function 
+## returns a "matrix" of functions to cache and retrieve matrix x, 
+## and retrieve its inverse from cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    #initialize inverse of matrix 
+    xinverse<-matrix()
+    
+    #cache matrix and reset the inverse of matrix to be NA
+    set<- function(y) {
+        x<<-y
+        xinverse<<-matrix()
+    }
+    #retrieve matrix
+    getmatrix<-function() x
+    
+    #cache inverse
+    setinverse<-function(inverse) xinverse<<-inverse
+    
+    #retrieve inverse
+    getinverse<-function() xinverse
+    
+    #returns a matrix of functions to cache and retrieve matrix, 
+    # and retrieve its inverse from cache
+    list(set=set, getmatrix=getmatrix, setinverse=setinverse, 
+         getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special matrix 
+## returned by makeCacheMatrix function. This function
+## returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    xinverse<- x$getinverse() #get inverse
+    
+    #if cached value for inverse has been calculated  
+    # (not NA), return the cached value
+    if(!is.na(xinverse)) {
+        message("getting cached matrix inverse")
+        return(xinverse)
+    }
+    #if inverse is NA (has not been calculated yet)
+    else {
+        #get cached matrix, and calculate matrix inverse
+        data<- x$getmatrix()
+        xinverse<- solve(data)
+        x$setinverse(xinverse) #cache inverse
+    }
+    xinverse
 }
